e9b4052e065b4a81c53bb3d74e85d6c8392fc954
TianyiZhao <zhaoti@umich.edu>
2020-05-04 01:37:57 -0400

    Initial Commit

---
+++ Chatter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chatter
+//
+//  Created by Tianyi Zhao on 5/4/20.
+//  Copyright © 2020 TianyiZhao20. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
